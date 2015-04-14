fe51c8c251191fc39a7944ae72beb3073f32e38a
salist <siarhei_mikulich@epam.com>
2015-04-14 10:51:51 +0300

    Add main page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,9 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+'use strict';
+/*
+*CSS Animated Hamburger Icon (click on hamburger)
+*/
+document.querySelector( '#nav-toggle' ).addEventListener( 'click', function() {
+    this.classList.toggle( 'active' );
+});
+
+
